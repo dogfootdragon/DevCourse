27e93f76df6857ff2739db9efb16baa5483df2a5
dogfootdragon <dogfootdragon@gmail.com>
2024-05-12 22:29:27 +0900

    vite로 react+ts 프로젝트 생성

---
+++ ReactExam/src/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ClassCom from './ClassCom';
+import FuncCom from './FuncCom';
+import TodoList from './Todolist';
+import MapTest from './MapTest';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return(
+    <div className='container'>
+      <TodoList></TodoList>
+      {/* <MapTest></MapTest> */}
+    </div>
+  )
+
+  // return(
+  //   <div className='component'>
+  //     <ClassCom></ClassCom> {/* 클래스형 컴포넌트 사용 */}
+  //     <FuncCom></FuncCom> { /* 함수형 컴포넌트 사용 */}
+  //   </div>
+  // )
+
+  // let name = "리액트";
+  // const style = { // css와 달리 카멜식으로 표기
+  //   backgroundColor : 'black',
+  //   color : 'white'
+  // }
+
+  // return (
+  //   <>
+  //   <div>
+  //     <h1 style = {style}>Hello, 
+  //     {
+  //       name === "리액트" ? (<h1>YES</h1>) : (<h1>NO</h1>)
+  //     }!</h1>
+  //     <p>반갑습니다.</p>
+  //     {/* 주석 작성 */}
+  //   </div>
+  //   </>
+  // )
+
+  // test port번호 리턴과 or 연산자
+  // const port = undefined;
+  // return(
+  //   <div>
+  //     {
+  //       port || '3000'
+  //     }
+  //   </div>
+  // )
+}
+
+export default App
---
+++ ReactExam/src/Todolist.tsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+
+type Todo = {
+  id : number;
+  text : string;
+  isChecked : boolean;
+}
+
+const TodoList : React.FC = () => {
+  const title : string = "오늘 할 일";
+  const [todos, setTodos] = useState<Todo[]>([
+    {id: 1, text: '공부하기', isChecked: false}, 
+    {id: 2, text: '잠자기', isChecked: false}, 
+    {id: 3, text: '미팅하기', isChecked: false}
+  ]); // 구조 분해 할당
+
+  const handleCheckedChange = (itemId : number) => {
+    setTodos((prevItems) =>
+      prevItems.map((item) => 
+        item.id == itemId ? {...item, isChecked : !item.isChecked} : item
+      )
+    )
+  }
+
+  return(
+    <div>
+      <h1>{title}</h1>
+      <p></p>
+      <div className='container'>
+        <div className='board'>
+          <ul>
+            {
+              todos.map((todo) => (
+                <li key={todo.id}>
+                  <input type="checkbox" 
+                  onChange={()=>{
+                    handleCheckedChange(todo.id);
+                  }}/>
+                  <span>
+                    {
+                      todo.isChecked ? 
+                      <del>{todo.text}</del>
+                      : <span>{todo.text}</span>
+                    }
+                  </span>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default TodoList;
